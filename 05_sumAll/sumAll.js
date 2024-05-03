e097e57172c1f867e2a5a6f36aeadf59d8ba452e
raza-t580-wsl <beheshti110100@gmail.com>
2024-05-03 06:09:32 +0500

    Upload the solved 05_sumAll Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end))    return "ERROR";
+let sum = 0;
+let loopStart = start < end ? start : end;
+let loopEnd = start < end ? end : start;
+for (loopStart; loopStart <= loopEnd; loopStart++)    sum += loopStart;
+    return sum;
 };
 
 // Do not edit below this line
