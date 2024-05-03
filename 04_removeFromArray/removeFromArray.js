89c788ede8c26f0f26a50c63dab9c372cbba9300
raza-t580-wsl <beheshti110100@gmail.com>
2024-05-03 05:50:36 +0500

    Solve Exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRemove) {
+for (const item of itemsToRemove)
+    {
+        while(array.indexOf(item) >= 0)
+            {
+                array.splice(array.indexOf(item), 1);
+            }
+    }
+    return array;
 };
 
 // Do not edit below this line
