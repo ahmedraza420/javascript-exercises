f9d5d84d399e62212efe61fce0483ecc85c2625f
raza-t580-wsl <beheshti110100@gmail.com>
2024-05-25 07:11:50 +0500

    Solve Exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let n1 = 0, n2 = 1, curr = 0;
+    if (index < 0) return 'OOPS';
+    if (index>0 && index<=2) return 1;  
+    for (let i = 2; i <= index; i++){
+                    curr = n1 + n2;
+                    n1 = n2;
+                    n2 = curr;
+        }
+    return curr;
 };
 
 // Do not edit below this line
