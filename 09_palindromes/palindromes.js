8df2cdc7e2c6222cef7195bc9a1542dc4dd813c2
raza-t580-wsl <beheshti110100@gmail.com>
2024-05-25 06:37:11 +0500

    Solve Exerise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let stringArray = str.split('');
+    let cleanArray = [];
+    // let cleanStr = cleanArray.join('');
+    for (char of str)
+        {
+            if ((((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z')) || (char >= 0 && char <= 9)) && char != ' ')
+            {
+                cleanArray.push(char.toLowerCase());
+            }
+        }
+        // console.log(cleanArray.reverse().join(''), cleanStr);
+    return cleanArray.join('') == cleanArray.reverse().join('');
 };
 
 // Do not edit below this line
