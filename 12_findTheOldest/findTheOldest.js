eab1a46073f268998017297b9486dcc0f6257b9e
raza-t580-wsl <beheshti110100@gmail.com>
2024-05-26 14:02:56 +0500

    Solve Exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrOfObj) {
+    return arrOfObj.reduce((oldest, curr) =>  (findAge(oldest.yearOfBirth, oldest.yearOfDeath) < findAge(curr.yearOfBirth, curr.yearOfDeath)) ? curr : oldest);
 };
 
+function findAge(birth, death) {
+    return death ? death - birth : (new Date().getFullYear()) - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
